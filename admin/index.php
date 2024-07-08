7de9463a2f8e08a42afc4c8771430ffe7909e2bb
sanjay-nakiya <fot1me01059@gmail.com>
2024-07-08 10:42:42 +0530

    admin update

--- admin/index.php
+++ admin/index.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
+    <title>Client</title>
 </head>
 <body>
-    <h1>Hello Admin</h1>
+    <h1>hello client</h1>
 </body>
 </html>
