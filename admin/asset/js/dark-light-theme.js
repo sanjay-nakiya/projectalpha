cce5b0912939f64544e50e13c4a654ac6fea0b92
sanjay-nakiya <fot1me01059@gmail.com>
2024-07-08 14:16:46 +0530

    massege

--- admin/asset/js/dark-light-theme.js
+++ admin/asset/js/dark-light-theme.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     $(".dark").click(function(){
       $("body").toggleClass("dark-mode");
-      $(".nl").toggleClass("navbar-primary");
       $(".sun-moon").toggleClass("bi-moon");
 
      // $("body").toggleClass("dark-mode");
---
+++ admin/footer.php
@@ -0,0 +1,3 @@
+<div class=" container btn viral-card  position-absolute bottom-0 start-50 translate-middle-x mb-2">
+	©Alpha
+</div>
