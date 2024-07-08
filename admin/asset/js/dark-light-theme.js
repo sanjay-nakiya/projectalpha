66f755e5840a11fd7cba1728441b58ff29d19579
parmar-viral <parmarviral8324@gmail.com>
2024-07-08 14:00:40 +0530

    update

---
+++ admin/asset/js/dark-light-theme.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".dark").click(function(){
+      $("body").toggleClass("dark-mode");
+      $(".nl").toggleClass("navbar-primary");
+      $(".sun-moon").toggleClass("bi-moon");
+
+     // $("body").toggleClass("dark-mode");
+      //$("body").toggleClass("dark-mode");
+      
+    });
+  });
--- admin/index.php
+++ admin/index.php
@@ -4,8 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Client</title>
+    <?php include 'css.php'; ?>
 </head>
 <body>
     <h1>hello client</h1>
+    <?php include 'menu.php'; ?>
+
+    <?php include 'js.php'; ?>
 </body>
 </html>
