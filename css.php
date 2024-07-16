c87afb357b1c8c604e5716c219d912718e389442
sanjay-nakiya <fot1me01059@gmail.com>
2024-07-16 16:32:19 +0530

    path and update theam

--- css.php
+++ css.php
@@ -1,3 +1,6 @@
+<?php 
+   $prn = "/projectalpha";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +9,7 @@
     <title></title>
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="assets/img/favicon/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="<?php echo $prn; ?>/assets/img/favicon/favicon.ico" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -15,25 +18,25 @@
       href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
       rel="stylesheet" />
 
-    <link rel="stylesheet" href="vendor/fonts/boxicons.css" />
+    <link rel="stylesheet" href="<?php echo $prn; ?>/vendor/fonts/boxicons.css" />
 
     <!-- Core CSS -->
-    <link rel="stylesheet" href="assets/vendor/css/core.css" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="assets/css/demo.css" />
+    <link rel="stylesheet" href="<?php echo $prn; ?>/assets/vendor/css/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="<?php echo $prn; ?>/assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="<?php echo $prn; ?>/assets/css/demo.css" />
 
     <!-- Vendors CSS -->
-    <link rel="stylesheet" href="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+    <link rel="stylesheet" href="<?php echo $prn; ?>/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
 
     <!-- Page CSS -->
     <!-- Page -->
-    <link rel="stylesheet" href="assets/vendor/css/pages/page-auth.css" />
+    <link rel="stylesheet" href="<?php echo $prn; ?>/assets/vendor/css/pages/page-auth.css" />
 
     <!-- Helpers -->
-    <script src="assets/vendor/js/helpers.js"></script>
+    <script src="<?php echo $prn; ?>/assets/vendor/js/helpers.js"></script>
     <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="assets/js/config.js"></script>
+    <script src="<?php echo $prn; ?>/assets/js/config.js"></script>
   
 </head>
 <body>
--- js.php
+++ js.php
@@ -11,18 +11,18 @@
 
 <!-- build:js assets/vendor/js/core.js -->
 
-<script src="assets/vendor/libs/jquery/jquery.js"></script>
-    <script src="assets/vendor/libs/popper/popper.js"></script>
-    <script src="assets/vendor/js/bootstrap.js"></script>
-    <script src="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
-    <script src="assets/vendor/js/menu.js"></script>
+<script src="<?php echo $prn; ?>/assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="<?php echo $prn; ?>/assets/vendor/libs/popper/popper.js"></script>
+    <script src="<?php echo $prn; ?>/assets/vendor/js/bootstrap.js"></script>
+    <script src="<?php echo $prn; ?>/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="<?php echo $prn; ?>/assets/vendor/js/menu.js"></script>
 
     <!-- endbuild -->
 
     <!-- Vendors JS -->
 
     <!-- Main JS -->
-    <script src="assets/js/main.js"></script>
+    <script src="<?php echo $prn; ?>/assets/js/main.js"></script>
 
     <!-- Page JS -->
 
