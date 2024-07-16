ba83e32cef7b7b21f7694e85f6f7a1a012847758
sanjay-nakiya <fot1me01059@gmail.com>
2024-07-16 15:07:58 +0530

    update

--- index.php
+++ index.php
@@ -7,7 +7,11 @@
 </head>
 <body>
   <h1>admin login -->  admin/index.php</h1>
-  <h1>cleint login -->  client/index.php</h1>
+  <h1>company login -->  company/index.php</h1>
+  <h1>client login -->  client/index.php</h1>
+  <h1>shop login -->  shop/index.php</h1>
+  <h1>customer login -->  customer/index.php</h1>
+
   
 </body>
 </html>
