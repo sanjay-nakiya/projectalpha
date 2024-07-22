172dd4efb49015eab63a129bf3c7e1548184f1bd
sanjay-nakiya <fot1me01059@gmail.com>
2024-07-22 16:26:36 +0530

    update new Theam

--- admin/css.php
+++ admin/css.php
@@ -8,17 +8,15 @@
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="<?php echo $pn; ?>/admin/assets/img/favicon/favicon.ico" />
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-      rel="stylesheet" />
+    
+<link rel="shortcut icon" type="image/x-icon" href="<?php echo $pn; ?>/admin/assets/img/favicon.jpg">
 
-    <!-- Core CSS -->
-    <link rel="stylesheet" href="<?php echo $pn; ?>/admin/assets/css/bootstrap.min.css" class="template-customizer-core-css" />
-    
-    
+<link rel="stylesheet" href="<?php echo $pn; ?>/admin/assets/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="<?php echo $pn; ?>/admin/assets/plugins/fontawesome/css/fontawesome.min.css">
+<link rel="stylesheet" href="<?php echo $pn; ?>/admin/assets/plugins/fontawesome/css/all.min.css">
+
+<link rel="stylesheet" href="<?php echo $pn; ?>/admin/assets/css/style.css">
 </head>
 <body>
     
--- admin/js.php
+++ admin/js.php
@@ -1,17 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-    
+
 </head>
+
 <body>
 
 
-    
-    <script src="<?php echo $pn; ?>/admin/assets/js/bootstap.bundle.min.js"></script>
-   
 
+
+    <script src="<?php echo $pn; ?>/admin/assets/js/jquery-3.6.0.min.js"></script>
+
+    <script src="<?php echo $pn; ?>/admin/assets/js/feather.min.js"></script>
+
+    <script src="<?php echo $pn; ?>/admin/assets/js/bootstrap.bundle.min.js"></script>
+
+    <script src="<?php echo $pn; ?>/admin/assets/js/script.js"></script>
 </body>
+
 </html>
