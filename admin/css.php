a29a162eeb6f5841cd1ae7a8597a6dd7d8b46f9f
sanjay-nakiya <fot1me01059@gmail.com>
2024-07-22 16:13:50 +0530

    basic bootstrap update

--- admin/css.php
+++ admin/css.php
@@ -15,26 +15,10 @@
       href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
       rel="stylesheet" />
 
-    <link rel="stylesheet" href="<?php echo $pn; ?>/admin/assets/vendor/fonts/boxicons.css" />
-
     <!-- Core CSS -->
-    <link rel="stylesheet" href="<?php echo $pn; ?>/admin/assets/vendor/css/core.css" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="<?php echo $pn; ?>/admin/assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="<?php echo $pn; ?>/admin/assets/css/demo.css" />
-
-    <!-- Vendors CSS -->
-    <link rel="stylesheet" href="<?php echo $pn; ?>/admin/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
-
-    <!-- Page CSS -->
-    <!-- Page -->
-    <link rel="stylesheet" href="<?php echo $pn; ?>/admin/assets/vendor/css/pages/page-auth.css" />
-
-    <!-- Helpers -->
-    <script src="<?php echo $pn; ?>/admin/assets/vendor/js/helpers.js"></script>
-    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
-    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="<?php echo $pn; ?>/admin/assets/js/config.js"></script>
-
+    <link rel="stylesheet" href="<?php echo $pn; ?>/admin/assets/css/bootstrap.min.css" class="template-customizer-core-css" />
+    
+    
 </head>
 <body>
     
--- admin/js.php
+++ admin/js.php
@@ -9,28 +9,9 @@
 <body>
 
 
-
+    
+    <script src="<?php echo $pn; ?>/admin/assets/js/bootstap.bundle.min.js"></script>
    
-    <!-- Core JS -->
-    <!-- build:js assets/vendor/js/core.js -->
 
-    <script src="<?php echo $pn; ?>/admin/assets/vendor/libs/jquery/jquery.js"></script>
-    <script src="<?php echo $pn; ?>/admin/assets/vendor/libs/popper/popper.js"></script>
-    <script src="<?php echo $pn; ?>/admin/assets/vendor/js/bootstrap.js"></script>
-    <script src="<?php echo $pn; ?>/admin/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
-    <script src="<?php echo $pn; ?>/admin/assets/vendor/js/menu.js"></script>
-
-    <!-- endbuild -->
-
-    <!-- Vendors JS -->
-
-    <!-- Main JS -->
-    <script src="<?php echo $path; ?>/admin/assets/js/main.js"></script>
-
-    <!-- Page JS -->
-
-    <!-- Place this tag in your head or just before your close body tag. -->
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
- 
 </body>
 </html>
