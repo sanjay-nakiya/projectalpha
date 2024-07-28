570b4a6adacfe62e45ddc1166a132bb67e342d18
sanjay-nakiya <fot1me01059@gmail.com>
2024-07-28 11:33:03 +0530

    header update

--- shop/header.php
+++ shop/header.php
@@ -3,7 +3,7 @@
             <div class="container-fluid"> <!--begin::Start Navbar Links-->
                 <ul class="navbar-nav">
                     <li class="nav-item"> <a class="nav-link" data-lte-toggle="sidebar" href="#" role="button"> <i class="bi bi-list"></i> </a> </li>
-                    <li class="nav-item d-none d-md-block"> <a href="#" class="nav-link">હોમ</a> </li>
+                    <li class="nav-item d-none d-md-block"> <a href="index.php" class="nav-link">હોમ</a> </li>
                     <li class="nav-item d-none d-md-block"> <a href="#" class="nav-link">સંપર્ક</a> </li>
                 </ul> <!--end::Start Navbar Links--> <!--begin::End Navbar Links-->
                 <ul class="navbar-nav ms-auto"> <!--begin::Navbar Search-->
@@ -13,7 +13,7 @@
                             <li class="user-header text-bg-primary"> <img src="../../dist/assets/img/user2-160x160.jpg" class="rounded-circle shadow" alt="User Image">
                                 <p>
                                 <?php echo ucwords($_SESSION['USERNAME']); ?>
-                                    <small>Member since Nov. 2023</small>
+                                    <small></small>
                                 </p>
                             </li> <!--end::User Image-->
                             </li> <!--end::Menu Body--> <!--begin::Menu Footer-->
