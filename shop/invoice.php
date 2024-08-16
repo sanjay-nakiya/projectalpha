a5f3fb9472ffdd8f19f5c08c626241ce6dadec0f
sanjay-nakiya <fot1me01059@gmail.com>
2024-08-16 09:24:19 +0530

    update

--- shop/invoice.php
+++ shop/invoice.php
@@ -24,6 +24,7 @@ if (3 == $_SESSION['ROLE']) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ડેશબોર્ડ</title>
     <?php include 'css.php'; ?>
+   
 </head>
 
 <body class="layout-fixed sidebar-expand-lg bg-body-tertiary">
@@ -66,7 +67,7 @@ if (3 == $_SESSION['ROLE']) {
 
                 <div class="container-fluid">
                 <!-- Invoice 1 - Bootstrap Brain Component -->
-
+                    
                    
                 </div>
                 <!--end::Container-->
@@ -81,8 +82,9 @@ if (3 == $_SESSION['ROLE']) {
     <!--end::App Wrapper-->
     <!--begin::Script-->
     <!--begin::Third Party Plugin(OverlayScrollbars)-->
-   
+    
     <?php include 'js.php'; ?>
+    <script src="assets/js/invoice.js"></script>
 </body>
 
 </html>
